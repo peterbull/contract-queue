2afe23d8ccb5fafb0f7bb167e9840bce78c98205
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2024-03-16 08:53:19 -0400

    WIP working through claude summarization dag

--- airflow/dags/generate_summaries_from_claude.py
+++ airflow/dags/generate_summaries_from_claude.py
@@ -1,44 +1,43 @@
 # Airflow 2.8.2
+import json
 import logging
 import os
-import re
-import tempfile
-from zipfile import BadZipfile
+import time
 
 import anthropic
+import boto3
+import botocore
 import pendulum
-import requests
-import textract
-import tiktoken
 from airflow.decorators import dag, task
-from app.models.models import (
-    Link,
-    NaicsCodes,
-    Notice,
-    OfficeAddress,
-    PlaceOfPerformance,
-    PointOfContact,
-    ResourceLink,
-)
+from app.models.models import Notice, ResourceLink
 from app.models.schema import NoticeBase, ResourceLinkBase
-from sqlalchemy import and_, create_engine, or_, select, text, update, values
+from sqlalchemy import create_engine, select, text, update
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 from tqdm import tqdm
 from typing_extensions import List, Optional
 
 logging.basicConfig(level=logging.INFO)
 
+# Database
 DATABASE_URL = os.environ.get("AIRFLOW__DATABASE__SQL_ALCHEMY_CONN")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-start_date = pendulum.datetime(2024, 3, 1)
 
-# params
+# S3
+S3_AWS_ACCESS_KEY_ID = os.environ.get("S3_AWS_ACCESS_KEY_ID")
+S3_AWS_SECRET_ACCESS_KEY = os.environ.get("S3_AWS_SECRET_ACCESS_KEY")
+S3_REGION_NAME = os.environ.get("S3_REGION_NAME")
+bucket_name = "sam-resource-links-haiku-summaries"
+
+# Dates
+start_date = pendulum.datetime(2024, 3, 1)
 prior_date = pendulum.now().subtract(days=1).strftime("%Y-%m-%d")
 
+# LLM params
 client = anthropic.Anthropic()
 bs = 5
-max_input_tokens = 50000
+max_input_tokens = 5000  # This number has to be very low right now, Anthropic API caps out at 1,000,000 tokens daily for the lowest tier currently
 max_output_tokens = 1000
 model = "claude-3-haiku-20240307"
 temperature = 0.0
@@ -59,16 +58,16 @@ default_args = {
 @dag(
     catchup=False,
     start_date=start_date,
-    schedule="0 6 * * *",
+    schedule=None,
     # schedule=None,
-    is_paused_upon_creation=True,
+    is_paused_upon_creation=False,
 )
 def generate_summaries_from_claude():
     @task()
-    def get_items_without_summaries(prior_date: str, max_input_tokens: int):
+    def get_resources_without_summaries(prior_date: str, max_input_tokens: int):
         with SessionLocal() as db:
             stmt = text(
-                """select id, text, notice_id from resource_links 
+                """select text from resource_links 
                             where notice_id in 
                                 (select id from notices
                                     where
@@ -88,12 +87,17 @@ def generate_summaries_from_claude():
                             file_tokens < :max_input_tokens
                         """
             )
-            results = db.execute(
-                stmt, params={"prior_date": prior_date, "max_input_tokens": max_input_tokens}
-            ).all()
+            results = (
+                db.execute(
+                    stmt, params={"prior_date": prior_date, "max_input_tokens": max_input_tokens}
+                )
+                .scalars()
+                .all()
+            )
 
         return results
 
+    @task()
     def claude_text_summarization(
         client: anthropic.Anthropic,
         doc_texts: str,
@@ -129,21 +133,81 @@ def generate_summaries_from_claude():
         ]
 
         with SessionLocal() as db:
-            for doc_text in doc_texts:
-                res = client.messages.create(
-                    model=model,
-                    max_tokens=max_output_tokens,
-                    temperature=temperature,
-                    system=system,
-                    messages=messages,
-                )
-                stmt = (
-                    update(ResourceLink)
-                    .where(ResourceLink.id == doc_text[0])
-                    .values(summary=res.content[0].text, summary_tokens=res.usage.output_tokens)
-                )
-                db.excute(stmt)
-                db.commit()
+            for doc_text in tqdm(doc_texts):
+                try:
+                    res = client.messages.create(
+                        model=model,
+                        max_tokens=max_output_tokens,
+                        temperature=temperature,
+                        system=system,
+                        messages=messages,
+                    )
+                except anthropic.APIConnectionError as e:
+                    logging.error("The server could not be reached")
+                    logging.error(e.__cause__)
+                    continue
+                except anthropic.RateLimitError as e:
+                    logging.error("A 429 status code was received; we should back off a bit.")
+                    time.sleep(60)  # sleep for 60 seconds
+                    continue
+                except anthropic.APIStatusError as e:
+                    logging.error("Another non-200-range status code was received")
+                    logging.error(f"Status code: {e.status_code}")
+                    logging.error(f"Response: {e.response}")
+                    continue
+
+                try:
+                    stmt = (
+                        update(ResourceLink)
+                        .where(ResourceLink.id == doc_text[0])
+                        .values(summary=res.content[0].text, summary_tokens=res.usage.output_tokens)
+                    )
+                    db.execute(stmt)
+                    db.commit()
+                except SQLAlchemyError as e:
+                    logging.error("An error occurred while updating the database.")
+                    logging.error(e)
+                    continue
+
                 logging.info(
                     f"Succesfully summarized {res.usage.input_tokens} token document in {res.usage.input_tokens} tokens"
                 )
+
+    @task()
+    def backup_summaries_to_s3(bucket_name: str = bucket_name, prior_date: str = prior_date):
+        with SessionLocal() as db:
+            stmt = (
+                select(ResourceLink.notice_id, ResourceLink.summary)
+                .join(Notice, ResourceLink.notice_id == Notice.id)
+                .where(Notice.postedDate == prior_date)
+            )
+            results = db.execute(stmt)
+            data = [{"notice_id": result[0], "summary": result[1]} for result in results]
+
+        s3_client = boto3.client(
+            "s3",
+            region_name=S3_REGION_NAME,
+            aws_access_key_id=S3_AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=S3_AWS_SECRET_ACCESS_KEY,
+        )
+        object_name = f"{bucket_name}/{prior_date}.json"
+        try:
+            json_data = json.dumps(data)
+            bytes_data = json_data.encode("utf-8")
+            s3_client.put_object(
+                Bucket=bucket_name,
+                Key=object_name,
+                Body=bytes_data,
+            )
+            logging.info(f"Successfully added {object_name} of length: {len(bytes_data)} to S3.")
+
+        except botocore.exceptions.ClientError as e:
+            print(f"Error: {e}")
+            return False
+
+        return True
+
+    resources_to_summarize = get_resources_without_summaries(
+        prior_date=prior_date, max_input_tokens=max_input_tokens
+    )
+    claude_text_summarization(client=client, doc_texts=resources_to_summarize)
